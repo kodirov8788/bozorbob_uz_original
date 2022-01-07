24174b3405577a06f2221c789dfa30a53d798847
kodirov8788 <kodirov8788@gmail.com>
2022-01-07 18:22:57 +0500

    sd;jaks

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   env: {
     // BASE_URL: "https://bozorbob-uz.vercel.app/",
-    // BASE_URL: "https://www.bozorbob.uz/",
+    BASE_URL: "https://www.bozorbob.uz/",
     // git config --global http.version HTTP/1.1
-    BASE_URL: "http://localhost:3000/",
+    // BASE_URL: "http://localhost:3000/",
     MONGODB_URL:
       "mongodb+srv://dunyoshop:84hfjGeD4ScN1G1W@cluster0.nscxl.mongodb.net/collection?retryWrites=true&w=majority",
     ACCESS_TOKEN_SECRET: "JG]d3~J7@.cmNdSkh=b~2$=ZwqbU8r4dsF+[K$T5uVaMPDn*<q",
